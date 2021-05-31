8668cac439398ce639795047c66b582351facf0d
Marcin Serafin <mks.serafin@hotmail.com>
2021-05-31 04:04:39 +0200

    Zmodyfikowano znacznie pliki do zadania księgowo-magazynowego

--- python03ksiegowosc/accountant.py
+++ python03ksiegowosc/accountant.py
@@ -1,105 +1,116 @@
 # KSIĘGOWY-MAGAZYN v1.0
 # -*- coding: UTF-8 -*-
 import sys
-import os
-path_1 = "plikInput.txt"
-path_2 = "plikMagazyn.txt"
-path_3 = "plikHistoria.txt"
+# import argparse
 
-def clear_screen():
-    if os.name == 'posix':  # dla mac i linux
-        _ = os.system('clear')
-    else:
-        _ = os.system('cls')  # dla windows
-    return
+# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - #
 
-def listingWarehouse():
-    for i in range(1, len(warehouse) + 1):
-        print("Aktualny stan magazynowy:")
-        print(warehouse[i][0], "\n")            # productID
-        print(warehouse[i][1], "\n")            # unitPrice
-        print(warehouse[i][2], "\n")            # quantity
-    return
-
-# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
-clear_screen()
-fileInput = open(path_1, "a+", encoding="utf8")
-fileWarhouse = open(path_2, "a+", encoding="utf8")
-fileHistory = open(path_3, "a+", encoding="utf8")
-
-dataInput: list = []       # dataInput[i] = (sys.argv[1], sys.argv[2], ...)
-warehouse: list = []       # warehouse[i] = (id_produktu, wartość_jedn, ilość)
-dataHistory: list = []     # dataHistory[i] = (argument1, argument2, ...)
-accountBalance = 1000000
+askHistory: list = []
+argvCurrent: list = []
+stockStatus: dict = {"jetson": 0, "raspberry": 0, "arduino": 0}
+accountBalance = 0
+operation = 0
+# take: list = []
 
 while 1:
-    inputData = str(sys.argv[1])
-    # for i in range(len(sys.argv) + 1):
-        dataInput.append([a][i] = str(sys.argv[i+1])
+    # take = [input("Podaj kolejne instrukcję: ")]
+    inData = str(sys.argv[1])  # pobieram nazwę operacji
+    fileIn = open("in.txt", "r+", encoding="utf8")
+    fileOut = open("out.txt", "w+", encoding="utf8")
 
-    fileInput.write(dataInput[len(dataInput)-1], "\n")
-    if inputData == "saldo" and len(sys.argv) == 4:
-        changeValue = int(sys.argv[2])
-        comment = str(sys.argv[3])
-        accountBalance += changeValue
-        dataHistory.append([inputData, accountBalance, comment])
-        fileHistory.write("{}".format(dataHistory[(len(dataHistory))-1]))
+    for i in range(1, len(sys.argv)):  # >>> ARGV
+        argvCurrent.append(sys.argv[i])
 
-    elif (inputData == "zakup" or inputData == "sprzedaż") and len(sys.argv) == 4:
-        productID = str(sys.argv[2])
-        unitPrice = int(sys.argv[3])
+    if inData == "saldo" and len(sys.argv) == 4:
+        accountBalance += int(sys.argv[2])  # >>> SALDO
+        fileOut.write("{}\n".format(accountBalance))  # >>> out.txt
+        print("{}\n".format(accountBalance))
+
+    elif (inData == "zakup" or inData == "sprzedaż") and len(sys.argv) == 4:
+        idName = str(sys.argv[2])
+        price = int(sys.argv[3])
         quantity = int(sys.argv[4])
-        if unitPrice < 0 or quantity <= 0:
-            print(" Podano absurdalne wartości dla ceny jednostkowej lub ilości.")
+        if price < 0 or quantity <= 0:
+            print("Błąd ceny lub ilości!")  # interrupt
             break
-        if inputData == "zakup":
-            if accountBalance < (unitPrice * quantity):
-                print(" Za mało kasy! Brakuje {} gr".format(
-                                int(accountBalance - (unitPrice * quantity)))
-            continue
+        if inData == "zakup":
+            if accountBalance < (price * quantity):
+                print("Brak środków!")  # interrupt
+                break
             else:
-                accountBalance -= (unitPrice * quantity)
-        elif inputData == "sprzedaż":
-            if True:   # sprawdz, czy nie ma w magazynie?
-                print(" Za mało asortymentu.")
-        continue
+                accountBalance -= (price * quantity)  # >>> SALDO
+                if stockStatus.get(idName, False):
+                    stockStatus[idName] += quantity  # >>> MAGAZYN
+                else:
+                    stockStatus[idName] = quantity  # +item
+        elif inData == "sprzedaż":
+            if stockStatus[idName] < quantity:
+                print("Brak w magazynie!")  # interrupt
+                break
             else:
-                pass    # jak jest, sprzedajemy!
-            accountBalance += (unitPrice * quantity)
-            warehouse.append([inputData, productID, unitPrice, quantity])
-        dataHistory.append([inputData, productID, unitPrice, quantity])
+                accountBalance += (price * quantity)  # >>> SALDO
+                stockStatus[idName] -= quantity  # >>> MAGAZYN
+                # if stockStatus[idName] == 0:
+                #    del stockStatus[idName]  # -item
+        for i in range(len(sys.argv) - 1):  # >>> out.txt
+            fileOut.write("{}\n".format(argvCurrent))
 
+    if inData == "saldo" or inData == "zakup" or inData == "sprzedaż":
+        askHistory.append(argvCurrent)  # >>> HISTORIA
+        stopLine = fileIn.readlines()
+        eof = len(stopLine)
+        del stopLine[eof-1]
+        fileIn.seek(0)
+        fileIn.writelines(stopLine)
+        fileIn.truncate()
+        for i in range(len(sys.argv) - 1):  # >>> in.txt
+            fileIn.write("{}\n".format(argvCurrent[i]))
+        fileIn.write("stop\n")
+        fileIn.close()
+        fileOut.close()
+        operation += 1
+        #break
 
-
-'''
-    elif inputData == "sprzedaż":
-        productID = str(sys.argv[2])
-        unitPrice = int(sys.argv[3])
-        quantity = int(sys.argv[4])
-        dataHistory.append([inputData, productID, unitPrice, quantity])
-        accountBalance += (unitPrice * quantity)
-'''
-
-        if unitPrice < 0 or quantity <= 0:
-            print(" Podano nierealne wartości dla ceny jednostkowej lub ilości.")
-            break
-        elif accountBalance < (unitPrice * quantity):
-            print(" Na koncie firmowym pozostało {:8d} gr".format(
-                int(accountBalance)))
-            print(" To za mało na dokonanie przedmiotowego zakupu.")
+    elif inData == "stop":
+        if operation > 0:
+            step = len(askHistory) - operation
+            for i in range(operation):
+                for j in range(4):
+                    print("{}\n".format(askHistory[step + 1 + i][j]))
+            operation = 0
         else:
-            accountBalance -= (unitPrice * quantity)
+            print("STOP")
+            break
 
-
-    elif inputData == "konto":
-
-
-    elif inputData == "magazyn":
-
-
-    elif inputData == "przegląd":
-
-
-    else:
-        print(" Wprowadzono nieprawidłowe dane! Przyjdź, jak się nauczysz :/")
+    elif inData == "konto" and len(sys.argv) == 2:
+        fileOut.write("{}\n".format(accountBalance))  # >>> out.txt
+        print("{}\n".format(accountBalance))
+        print("{}\n".format(stockStatus.items()))
         break
+
+    elif inData == "magazyn" and len(sys.argv) > 1:
+        for i in range(2, len(sys.argv)):
+            for k, v in stockStatus.items():
+                if str(sys.argv[i]) == stockStatus.get(k):  # interrupt
+                    print("{}: {}\n".format(k, v))
+                    fileOut.write("{}: {}\n".format(k, v))  # >>> out.txt
+        #break
+
+    if inData == "przegląd":
+        for i in range(int(sys.argv[2]), int(sys.argv[3]) + 1):  # >>> out.txt
+            for j in range(len(askHistory[i])):
+                print("{}\n".format(askHistory[i][j]))
+                fileOut.write("{}\n".format(askHistory[i][j]))
+        #break
+    break
+
+'''
+argv = argparse.ArgumentParser(description="Pobranie argumentów z konsoli")
+argv.add_argument('', '--1', help="Wartość argv[1]",
+                  type=str, required=False)
+argv.add_argument('', '--2', help="Wartość argv[2]",
+                  type=str, required=False)
+argv.add_argument('', '--3', help="Wartość argv[3]",
+                  type=str, required=False)
+args = argv.parse_args()
+'''
